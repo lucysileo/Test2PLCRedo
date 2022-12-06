31b9cdef0f07a46b3569a90d2336189ca6c361f2
lucysileo <64927052+lucysileo@users.noreply.github.com>
2022-12-05 19:41:02 -0500

    Create testoutput.py

---
+++ testoutput.py
@@ -0,0 +1,24 @@
+
+Edward Sileo
+Sun, Dec 4, 3:46 PM (1 day ago)
+to me
+
+tiny x;
+
+small y;
+
+medium a;
+
+large z;
+
+A = 5;
+
+B = A ADD 7 MUL OPENPAREN C DIV 9 CLOSEPAREN;
+
+Error: Subexpression Syntax Error in line Z = 7 ADD 7 ADD 7 ADD 7 MUL;
+
+COND A GTE 5:
+
+REPEAT OPENPAREN A ADD 12 CLOSEPAREN LT OPENPAREN B SUB 7 CLOSEPAREN:
+
+Error: Paren Count Mismatch in line COND B EQ OPENPAREN 5 ADD 7 CLOSEPAREN CLOSEPAREN:
